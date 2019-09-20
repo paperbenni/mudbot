435828afcf50d22cce33687f13eb247ef72e0c0c
paperbenni <paperbenni@gmail.com>
2019-09-20 10:23:34 +0200

    mud class

--- discordmud.py
+++ discordmud.py
@@ -5,6 +5,7 @@ import asyncio
 import os
 import threading
 import re
+import mud
 
 
 def getvar(string, defaultstr='none'):
@@ -18,9 +19,7 @@ def getvar(string, defaultstr='none'):
             return defaultstr
 
 
-discordtoken = getvar('discord')
-mudname = getvar('mudname', 'discorder')
-mudpass = getvar('mudpass', 'discord')
+discordtoken = getvar(discord)
 
 
 def discordtext(input):
@@ -37,38 +36,17 @@ def splitCount(string, length):
 
 
 client = discord.Client()
-tn = Telnet('aardmud.org', 4000)
-tn.read_until(b'adventurer?')
-time.sleep(1)
-tn.write(b'discorder\n')
-time.sleep(1)
-tn.write(b'discord\n')
-time.sleep(1)
-tn.write(b'\n')
-mudchannels = []
+muds = []
 mudstring = ''
 
 
-def outputrec():
-    global tn
-    global mudstring
-    print('global outputrec')
-    while not client.is_closed():
-        output = url = re.sub('\\n$', '', tn.read_until(b'\n').decode(
-            'utf-8'))
-
-        output = re.sub('\[.;36m!', '᥅', output)  # unkillable mob
-        output = re.sub('\[.;34m\+', '🔒', output)  # closed door
-        output = re.sub('\[.;33m<', '▼', output)  # closed down exit
-        output = re.sub('\[.;33m>', '▲', output)  # closed up exit
-        output = re.sub('\[.;35m#', '♥', output)  # you icon
-        
-        print(output)
-        mudstring += output
-
-
-threadd = threading.Thread(name='messager', target=outputrec)
-threadd.start()
+def mudtext(mud):
+    output = re.sub('\[.;36m!', '᥅', mud)  # unkillable mob
+    output = re.sub('\[.;34m\+', '🔒', output)  # closed door
+    output = re.sub('\[.;33m<', '▼', output)  # closed down exit
+    output = re.sub('\[.;33m>', '▲', output)  # closed up exit
+    output = re.sub('\[.;35m#', '♥', output)  # you icon
+    return output
 
 
 @client.event
@@ -80,32 +58,40 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
         return
-    global mudchannels
-    global mudstring
-    if message.content == '!mud':
-        print("added new channel")
-        if not message.channel in mudchannels:
-            mudchannels.append(message.channel)
-            await message.channel.send('mud channel {} added'.format(message.channel.name))
-        return
-    if not message.channel in mudchannels:
-        return
-    # prevent players crashing the game
-    if message.content == 'quit':
-        return
-    if message.content != '!':
-        print('sending command')
-        if message.content == '!!':
-            tn.write(b'\n')
 
-        tn.write(bytes(message.content, encoding='utf8') + b'\n')
-    await asyncio.sleep(0.5)
-    for i in mudchannels:
-        if len(mudstring) >= 1600:
-            for u in stringsplit(mudstring, 1000):
-                await i.send(discordtext(u))
+    global muds
+
+    alreadyadded = False
+
+    for i in muds:
+        if message.channel == i.getchannel():
+            alreadyadded = True
+            activemud = i
+            break
+
+    if str(message.content).startswith('!mud'):
+        if not alreadyadded:
+            msgargs = str(message.content).split(' ')
+            thismud = mud.Mud()
+            if len(msgargs) >= 3:
+                thismud.connect(msgargs[1], msgargs[2])
+            else:
+                thismud.connect()
+            muds.append(thismud)
+            thismud.read().start()
         else:
-            if not discordtext(mudstring) == '``````':
-                await i.send(discordtext(mudstring))
-    mudstring = ''
+            await message.channel.send('channel already added')
+        return
+
+    activemud.execute(message.content)
+    await asyncio.sleep(0.5)
+    mudstring = activemud.getstring()
+    if len(mudstring) >= 1600:
+        for u in stringsplit(mudstring, 1000):
+            await i.send(discordtext(u))
+    else:
+        if not discordtext(mudstring) == '``````':
+            await i.send(discordtext(mudstring))
+    activemud.resetmsg()
+
 client.run(discordtoken)
--- mud.py
+++ mud.py
@@ -1,12 +1,23 @@
 import telnetlib
 import time
+import threading
 
 
 class Mud:
+
     def __init__(self):
         self.logged_in = False
+        self.messages = ''
 
-    def connect(self, host='aardmud.org', port='4000'):
+    def connect(self, host=False, port=False):
+        if host:
+            if not port:
+                port = 23
+        else:
+            host = 'aardmud.org'
+            port = 4000
+        self.host = host
+        self.port = port
         self.tn = telnetlib.Telnet(host, port)
 
     def login(self, username=b'discorder', password=b'discord'):
@@ -24,10 +35,33 @@ class Mud:
             self.tn.write(b'\n')
 
     def execute(self, command):
+        execommand = command
+        if command == '!!':
+            execommand == b'\n'
+
         try:
-            self.tn.write(bytes(command, encoding='utf8'))
+            self.tn.write(bytes(execommand + '\n', encoding='utf8'))
         except:
-            self.login(self.username, self.password)
+            self.connect(self.host, self.port)
             time.sleep(0.2)
             print(self.username, 'reconnecting')
             self.execute(command)
+
+    def read(self):
+        def reader(self):
+            while True:
+                output = self.tn.read_until(b'\n')
+                self.messages += output
+        return threading.Thread(target=reader)
+
+    def getstring(self):
+        return self.messages
+
+    def resetmsg(self):
+        self.messages = ''
+
+    def setchannel(self, channel):
+        self.channel = channel
+
+    def getchannel(self):
+        return self.channel
